054d62bbed03dac3b5529acb9495200f2a0454cc
Gian Carlo Vegas R <giancarlovr@gmail.com>
2016-08-10 18:10:30 -0500

    components done

--- e11/gulpfile.js
+++ e11/gulpfile.js
@@ -1,4 +1,104 @@
 /**
  * Created by gvegas on 3/8/2016.
  */
-'use strict';
+/**
+ * Dependencies
+ */
+var browserSync = require('browser-sync'),
+    gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    taskListing = require('gulp-task-listing'),
+    util = require('gulp-util');
+
+/**
+ * List the available gulp tasks
+ */
+gulp.task('default', taskListing);
+
+/**
+ * Compile .scss to css
+ */
+gulp.task('compile-sass', function () {
+    log('Compiling Sass --> CSS');
+
+    return gulp
+        .src('src/sass/**/*.scss')
+        //.src('src/sass/styles.scss')
+        //.pipe($.plumber()) // exit gracefully if something fails after this
+        .pipe(sass())
+        //        .on('error', errorLogger) // more verbose and dupe output. requires emit.
+        .pipe(autoprefixer({browsers : ['last 2 version', '> 5%']}))
+        .pipe(gulp.dest('site/css'));
+});
+
+/**
+ * Start dev server
+ */
+gulp.task('dev-serv-start', function () {
+    startBrowserSync();
+});
+
+function startBrowserSync(isDev) {
+
+    log('Starting BrowserSync on port 3000');
+
+    // If build: watches the files, builds, and restarts browser-sync.
+    // If dev: watches sass, compiles it to css, browser-sync handles reload
+    gulp.watch(['src/sass/**/*.scss'], ['compile-sass'/*, 'browserSyncReload-dev'*/])
+        .on('change', changeEvent);
+
+    var options = {
+        port           : 3000,
+        server         : 'site', /*{
+         baseDir : 'site'
+         },*/
+        files          : [
+            'site/*.html',
+            'site/css/*.*',
+            'site/js/**/*.js'
+        ],
+        ghostMode      : { // these are the defaults t,f,t,t
+            clicks   : true,
+            location : false,
+            forms    : true,
+            scroll   : true
+        },
+        injectChanges  : true,
+        logFileChanges : true,
+        logLevel       : 'info',
+        logPrefix      : 'gk',
+        notify         : true,
+        reloadDelay    : 500
+    };
+
+    browserSync(options);
+}
+
+/**
+ * When files change, log it
+ * @param  {Object} event - event that fired
+ */
+function changeEvent(event) {
+    var srcPattern = new RegExp('/.*(?=/src)/');
+    log('File ' + event.path.replace(srcPattern, '') + ' ' + event.type);
+}
+
+/**
+ * Log a message or series of messages using chalk's blue color.
+ * Can pass in a string, object or array.
+ */
+function log(msg) {
+    if (typeof(msg) === 'object') {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                util.log(util.colors.blue(msg[item]));
+            }
+        }
+    }
+    else {
+        util.log(util.colors.blue(msg));
+    }
+}
--- e11/site/js/app.js
+++ e11/site/js/app.js
@@ -2,3 +2,103 @@
  * Created by gvegas on 3/8/2016.
  */
 'use strict';
+
+var alertsConfigFields = {
+    recipients: true,
+    events: true,
+    frequency: true
+}
+
+var configuredAlerts = {
+    recipients: 'someone@somewhere.com, someoneelse@somewhereelse.com, noone@nowhere.com',
+    includeAdversary: true,
+    notificationEvents: [
+        'Transfers',
+        'Agendas'
+    ]
+}
+
+jQuery(document).ready(function () {
+    $('#btnAddAlert').click(function () {
+        if($('#selectlistAlerts').is(':visible'))
+            $('#selectlistAlerts').hide();
+        else
+            $('#selectlistAlerts').show();
+    });
+
+    $('.checkbox').click(function () {
+        var controlIcon = $(this).find('i');
+        if(controlIcon.hasClass('fa-square-o')) {
+            controlIcon.removeClass();
+            controlIcon.addClass('fa fa-check-square checkbox__icon');
+        } else {
+            controlIcon.removeClass();
+            controlIcon.addClass('fa fa-square-o checkbox__icon');
+        }
+    });
+
+    $('.radio-button').click(function () {
+        var controlIcon = $(this).find('i');
+        if(controlIcon.hasClass('fa-circle-o')) {
+            $('.radio-button i').removeClass('fa-dot-circle-o').addClass('fa-circle-o');
+            controlIcon.addClass('fa fa-dot-circle-o radio-button__icon');
+        } else {
+            controlIcon.removeClass();
+            controlIcon.addClass('fa fa-circle-o radio-button__icon');
+        }
+    });
+
+    $('#txtNotificationEventDisplay').click(function () {
+        if($('#selectEventoOptions').is(':visible'))
+            $('#selectEventoOptions').hide();
+        else
+            $('#selectEventoOptions').show();
+    });
+
+    $('.multiselect__list-item').click(function () {
+        $(this).addClass("multiselect__list--selected");
+
+        var newTag = $('<div class="tag-label"><span class="tag-label__text">' + $(this).html() + '</span><span class="tag-label__icon"><i class="fa fa-times-circle"></i></span></div>');
+        newTag.on('click', function () {
+            $(this).remove();
+        });
+
+        $('#txtNotificationEventDisplay').append(newTag);
+        $('#selectEventoOptions').hide();
+    });
+
+    $('#btnAlertFrequency').click(function () {
+        if($('#selectlistAlertFrequency').is(':visible'))
+            $('#selectlistAlertFrequency').hide();
+        else
+            $('#selectlistAlertFrequency').show();
+    });
+
+    $('.alertFrequencyItem').click( function () {
+        var text = $(this).html();
+        $('#btnAlertFrequency .select-option__display-text').html(text);
+        $('#selectlistAlertFrequency').hide();
+    });
+
+    $('#chkFrequencyRealtime').click(function () {
+        if($('#itemsRealTime').is(':visible'))
+            $('#itemsRealTime').hide();
+        else
+            $('#itemsRealTime').show();
+    });
+
+    $('#chkFrequencyDaily').click(function () {
+        if($('#itemsDaily').is(':visible'))
+            $('#itemsDaily').hide();
+        else
+            $('#itemsDaily').show();
+    });
+
+    $('#chkFrequencyWeekly').click(function () {
+        if($('#alertFrequencyWeeklyOptions').is(':visible'))
+            $('#alertFrequencyWeeklyOptions').hide();
+        else
+            $('#alertFrequencyWeeklyOptions').show();
+    });
+    
+});
